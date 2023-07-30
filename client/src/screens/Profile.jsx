ad88ebf94eec9926a1e1a6dd7ca32424450dde89
AbdifataghOsman2 <abdifatahosman205@gmail.com>
2023-07-29 23:33:51 -0700

    first commit

--- client/src/screens/Profile.jsx
+++ client/src/screens/Profile.jsx
@@ -49,7 +49,7 @@ export default function Profile(props) {
         </button>
         {isOpenTwo && (
           <div className="dropdown-menu">
-            {/* content for the second container */}
+            {/* content for the second container*/}
           </div>
         )}
       </div>
